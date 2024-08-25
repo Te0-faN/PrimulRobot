6496c5fc34f3652e4ff0c5ffdf7ac6fe1891fec3
Wardeme Microsoft <isidor.herea@gmail.com>
2024-08-25 16:50:57 +0300

    chesti

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testsdadadasda.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testsdadadasda.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name = "TestOP")
-public class Test extends LinearOpMode
+public class Testsdadadasda extends LinearOpMode
 {
     @Override
     public void runOpMode()
